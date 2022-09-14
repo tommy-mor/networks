1a79e79d1da1dd47573177a2af8fa93dfe79943a
tommy <thmorriss@gmail.com>
2022-09-14 15:21:15 -0400

    smal

--- src/networks/core.clj
+++ src/networks/core.clj
@@ -7,17 +7,17 @@
             [clojure.tools.cli :refer [parse-opts]])
   (:gen-class))
 
-(comment 
+(defn wordle [{:keys [hostname username encrypted port]}] 
   (def words (clojure.string/split (slurp (clojure.java.io/resource "project1-words.txt")) #"\r\n"))
   (def protocol (gloss/compile-frame (gloss/string :utf-8 :delimiters ["\n"])))
-  (def c @(tcp/client {:host "proj1.3700.network" :port 27994 :ssl? true :insecure? true}))
+  (def c @(tcp/client (cond-> {:host hostname :port port}
+                        encrypted (assoc :ssl? true :insecure? true))))
   (def read-stream (io/decode-stream c protocol))
   
   (defn request [body]
     @(s/put! c (str (json/write-str body) "\n"))
     (let [x @(s/take! read-stream)
           r (json/read-str x :key-fn keyword)]
-      (println "received" r)
       r))
 
   (def session-id (:id (request {:type "hello" :northeastern_username "morriss.t"})))
@@ -51,10 +51,7 @@
                   (request {:type "guess" :id session-id :word (calc-response (:guesses resp))}))
                  resp)))
   
-  
-  
-  
-  )
+  (println (:flag final)))
 
 (def cli-options
   [["-p" "--port PORT" "port number" :default :unspecified :parse-fn #(Integer/parseInt %) :id :port]
@@ -64,12 +61,12 @@
   (def opts (parse-opts args cli-options))
   (assert (= 2 (count (:arguments opts))))
   
-  (prn {:hostname (-> opts :arguments first)
-        :username (-> opts :arguments second)
-        :encrypted (-> opts :options :encrypted)
-        :port (let [port (-> opts :options :port)
-                    encrypted (-> opts :options :encrypted)]
-                (cond
-                  (and encrypted (= port :unspecified)) 27994
-                  (= port :unspecified) 27993
-                  :else port))}))
+  (wordle {:hostname (-> opts :arguments first)
+           :username (-> opts :arguments second)
+           :encrypted (-> opts :options :encrypted)
+           :port (let [port (-> opts :options :port)
+                       encrypted (-> opts :options :encrypted)]
+                   (cond
+                     (and encrypted (= port :unspecified)) 27994
+                     (= port :unspecified) 27993
+                     :else port))}))
