f7ca12f16fefd9d66cfb1a5f9783b37e50a56463
tommy <thmorriss@gmail.com>
2022-09-24 16:19:58 -0400

    progress

--- src/networks/core.clj
+++ src/networks/core.clj
@@ -19,24 +19,38 @@
                  :password "3sNurnEDZX7Q6aHWvdO1"})
 
 (def control (atom nil))
+(def data (atom nil))
+(def logged-in (atom false))
+
 (defn reset-connection []
   (if (nil? @control)
     (do
       (reset! control @(tcp/client {:host url
                                     :port 21}))
+      (reset! logged-in false)
       (slurp @(s/take! @control))
       #_(reset! read-control (io/decode-stream @control protocol)))
     (do
       (s/close! @control)
+      (when @data (s/close! @data))
       (reset! control nil)
+      (reset! data nil)
       (reset-connection))))
 
 (comment (reset-connection))
 
+(defn take-stream-one []
+  @(s/try-take! @control :drained 500 :empty))
+
 (defn take-stream []
-  (let [r (slurp @(s/take! @control :empty))]
-    (println "RECEIVED" r)
-    r))
+  (let [recieved
+        (take-while (partial not= :empty)
+                    (repeatedly take-stream-one))]
+    (def r recieved)
+    (println "received " (count r))
+    (let [final (slurp (last recieved))]
+      (println "RECEIVED" final)
+      final)))
 
 ;; TODO some requests have two responses
 (defn request [cmd & args]
@@ -48,10 +62,16 @@
   (clojure.string/split (take-stream) #" " 2))
 
 (defn login []
-  (def req (request "USER" (:username login-data)))
-  (assert (= "331" (first req)))
-  (def pwreq (request "PASS" (:password login-data)))
-  (assert (= "230" (first pwreq))))
+  (when-not @logged-in
+    (def req (request "USER" (:username login-data)))
+    (assert (= "331" (first req)))
+    (def pwreq (request "PASS" (:password login-data)))
+    (assert (= "230" (first pwreq)))
+    
+    (assert (= "200" (first (request "TYPE" "I"))))
+    (assert (= "200" (first (request "MODE" "S"))))
+    (assert (= "200" (first (request "STRU" "F"))))
+    (reset! logged-in true)))
 
 (defn shift-add [top bottom]
   (+ (bit-shift-left top 8)
@@ -68,24 +88,41 @@
         ipnums (take 4 numbers)
         portnums (map #(Integer/parseInt %) (drop 4 numbers))
         topbits (first portnums)
-        lowerbits (second portnums)]
+        lowerbits (second portnums)
+        port (shift-add topbits lowerbits)]
+    (println port)
     {:host (clojure.string/join "." ipnums)
-     :port (shift-add topbits lowerbits)}))
+     :port port}))
+
+(defn respond-227 [req]
+  (reset! data @(tcp/client (parse-pasv-ip (second req))))
+  @data)
 
 (defn open-data-channel []
   (def pasvreq (request "PASV"))
-  (tcp/client (parse-pasv-ip (second pasvreq))))
+  (if (= "227" (first pasvreq))
+    (respond-227 pasvreq)
+    (println "not making new channel...")))
 
-(defn ls []
+(defn ls [dirr]
+  (def dirr dirr)
   (login)
-  (assert (= "200" (first (request "TYPE" "I"))))
-  (assert (= "200" (first (request "MODE" "S"))))
-  (assert (= "200" (first (request "STRU" "F"))))
-  (def data @(open-data-channel))
-  (assert (= "150" (first (request "LIST" "."))))
-  (def ls-result (slurp @(s/take! data)))
+  
+  (open-data-channel)
+  
+  (let [list-req (request "LIST" dirr)]
+    (case (first list-req)
+      "150" :okay
+      "227" (respond-227 list-req)
+      "226" :do-nothing?))
+  
+  (println "closed?" (s/closed? @data))
+  
+  (def ls-result (slurp @(s/take! @data)))
+  (println "closed?" (s/closed? @data))
   (clojure.string/split ls-result #"\r\n"))
 
+(comment (ls "."))
 
 (def cli-options [])
 
@@ -105,8 +142,6 @@
 (defmulti ftp (fn [fst & r] (keyword fst)))
 (defmethod ftp :ls [_ url]
   (println url))
-
-(ftp "ls" url)
 
 (defn -main [& args]
   (def opts (parse-opts args cli-options))
