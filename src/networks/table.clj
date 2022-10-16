da310bc22c5fac93c466b50f0999910e5416a3fb
tommy <thmorriss@gmail.com>
2022-10-16 18:20:40 -0400

    forwarding one packet works i think

--- src/networks/table.clj
+++ src/networks/table.clj
@@ -5,6 +5,7 @@
 (defn oneify-ip [ip]
   (clojure.string/join "." (let [d (clojure.string/split ip #"\.")]
                              (conj (vec (butlast d)) (str (dec (Integer. (last d))))))))
+
 (assert (= "192.168.0.1" (oneify-ip "192.168.0.2")))
 
 (defn send-message [{:keys [socket ip port]} msg]
@@ -18,8 +19,10 @@
   (atom (list {:port 59484, :ip "192.168.0.2", :purpose "cust"}
               {:port 51646, :ip "172.168.0.2", :purpose "cust"})))
 
-;; neighber -> "where that neighbor can send things to.."
-(def routing-table (atom {}))
+(defn ip->neighbor [ip]
+  (first (filter (comp #{ip} :ip) @neighbors)))
+
+(def routing-table (atom []))
 (def all-messages (atom []))
 
 (def msg {:type "update",
@@ -44,6 +47,8 @@
   "add entry to forwarding table"
   "potentially send to neighboring routers"
 
+  (swap! routing-table conj [(:src msg) (:msg msg)])
+
   (doall (for [neighbor (other-neighbors (:src msg))]
            (send-message
             neighbor
@@ -55,6 +60,50 @@
                       (dissoc :localpref
                               :origin ;; TODO there is more logic to be done here
                               :selfOrigin))}))))
+
+
+(def data1 {:src "172.168.0.25", :dst "192.168.0.25", :type "data", :msg {:ignore "this"}})
+(def data2 {:src "192.168.0.25", :dst "172.168.0.25", :type "data", :msg {:ignore "this"}})
+
+(defn ip->int [ip]
+  (apply + (map (fn [base coef] (bit-shift-left base coef))
+                (map #(Integer/parseInt %) (clojure.string/split ip #"\."))
+                [24 16 8 0])))
+
+(assert (= (ip->int "255.255.255.255") 2r11111111111111111111111111111111))
+(assert (= (ip->int "255.255.255.0") 2r11111111111111111111111100000000))
+(assert (= (ip->int "255.255.0.0") 2r11111111111111110000000000000000))
+
+(defn entry-applies? [ip entry]
+  (= (bit-and (ip->int (:network entry))
+              (ip->int (:netmask entry)))
+     (bit-and (ip->int ip)
+              (ip->int (:netmask entry)))))
+
+(assert (entry-applies? "192.168.0.25" {:network "192.168.0.0" :netmask "255.255.255.0"}))
+(assert (not (entry-applies? "192.168.2.25" {:network "192.168.0.0" :netmask "255.255.255.0"})))
+(assert (entry-applies? "192.168.2.25" {:network "192.168.0.0" :netmask "255.255.0.0"}))
+
+(defn matches [ip]
+  "gets the routes in the table that are apply to this ip"
+
+  (filter (comp (partial entry-applies? ip) second) @routing-table))
+
+(def msg data1)
+
+
+
+(defmethod process-message :data [msg]
+  (def potential-routes (matches (:dst msg)))
+
+  (cond
+    (= 1 (count potential-routes))
+    (let [[ip _] (first potential-routes)] (send-message (ip->neighbor ip)
+                                                         msg)))
+  "scenario 1: does not have route, gives no route message"
+  "scenario 2: exactly one possible route, forward properly"
+  "scenario 3: multiple routes, do longest prefix match"
+  "check for legality: if source|dest is customer: send data. if source is peer|providor AND dest peer|providor, then drop message . send no route")
 
 (comment
   (process-message update-message))
